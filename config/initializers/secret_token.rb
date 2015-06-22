f9fa5dd16c740d041afffee5d05f02b26a1c1e62
Ban <le.van.ban@framgia.com>
2015-06-22 18:27:56 +0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HelloApp::Application.config.secret_key_base = '9286b28df8374ffed46598a2f410a2c66ebf40f0efb88b406a9cee35b739733293603261a26957e17a88be76433457504535fb7a4dfcd73c4eecba09e6111005'
