77ae5a1f97fa354010f7cb244cd0861a7ce0659b
Ban <le.van.ban@framgia.com>
2015-06-22 19:13:44 +0700

    add notes

--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,6 @@ HelloApp::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  root "notes#index"
+  resources :notes
 end
